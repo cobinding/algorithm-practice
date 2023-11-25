d431d8c131174518eba3e0b8b5264fe5af016a8f
sebin chu <102461290+cobinding@users.noreply.github.com>
2023-11-25 20:13:26 +0900

    [Silver IV] Title: Project Teams, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/20044. Project Teams/Project Teams.py
+++ 백준/Silver/20044. Project Teams/Project Teams.py
@@ -1,8 +1,7 @@
 import sys; input = sys.stdin.readline
 
 n = int(input().rstrip())
-students = list(map(int, input().rstrip().split()))
-students.sort()
+students = sorted(map(int, input().rstrip().split()))
 
 temp = []
 for i in range(n):
