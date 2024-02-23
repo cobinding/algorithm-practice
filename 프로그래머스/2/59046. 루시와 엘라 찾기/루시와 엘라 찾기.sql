1b1e02e7ebbf1cd928bbac07ecfa02117763630a
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-02-23 23:17:12 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,3 @@
+SELECT animal_id, name, sex_upon_intake
+from animal_ins
+where name like "Lucy" or name like "Ella" or name like "Pickle" or name like "Rogan" or name like "Sabrina" or name like "Mitty"
