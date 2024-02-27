028e8b0c6586ce4946102500cd973828b46ff5bc
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-02-28 00:47:10 +0900

    [Silver III] Title: 가희와 파일 탐색기, Time: 1232 ms, Memory: 145476 KB -BaekjoonHub

--- 백준/Silver/22232. 가희와 파일 탐색기/가희와 파일 탐색기.py
+++ 백준/Silver/22232. 가희와 파일 탐색기/가희와 파일 탐색기.py
@@ -3,11 +3,8 @@ import sys;input=sys.stdin.readline
 n,m=map(int, input().split())
 
 jo_test = [input().rstrip() for _ in range(n)]
-ext = dict()
+ext = set(input().rstrip() for _ in range(m))
 
-for _ in range(m):
-    key = input().rstrip()
-    ext[key] = 1
 jo_test_split = []
 for item in jo_test:
     jo_test_split.append(item.split("."))
@@ -25,16 +22,6 @@ for i in range(n-1):
             tmp = jo_test_split[i]
             jo_test_split[i] = jo_test_split[i+1]
             jo_test_split[i+1] = tmp
-
-        """   # 1, 2번 정렬이 안되고, 확장자 사전순으로 정렬해야 하는 경우
-            if jo_test_split[i][1] in ext and  jo_test_split[i+1][1] in ext:
-                l = min(len(jo_test_split[i][1]), len(jo_test_split[i+1][1]))
-                for j in range(l):
-                    if ord(jo_test_split[i][1][j]) > ord(jo_test_split[i+1][1][j]):
-                        tmp = jo_test_split[i]
-                        jo_test_split[i] = jo_test_split[i+1]
-                        jo_test_split[i+1] = tmp
-                        break"""
            
 for item in jo_test_split:
     print(item[0]+"."+item[1])
