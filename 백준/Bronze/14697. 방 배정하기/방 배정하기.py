4bb64b495345f165dadb0c3521375258a9a364cc
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-03-14 22:56:54 +0900

    [Bronze II] Title: 방 배정하기, Time: 48 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -2,9 +2,9 @@ a,b,c,students=map(int,input().split())
 
 # a*i + b*j + c*k = students
 ans = 0
-for i in range(students//a):
-    for j in range(students//b):
-        for k in range(students//c):
+for i in range(students//a+1):
+    for j in range(students//b+1):
+        for k in range(students//c+1):
             if students == (a*i + b*j + c*k):
                 print(1)
                 exit()
