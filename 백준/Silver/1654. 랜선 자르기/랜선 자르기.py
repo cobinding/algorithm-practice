8f6e4ed7195b0796dfdcb53c26fc8aa64a91c184
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-04-03 17:00:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 124 ms, Memory: 110540 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,21 @@
+import sys;input=sys.stdin.readline
+
+k,n=map(int,input().split())
+lan=[int(input()) for _ in range(k)]
+
+# 가장 짧은 길이를 1, 가장 긴 길이를 max(lan)으로 두고 이분 탐색한다.
+left,right=1,max(lan)
+ans=0
+while left <= right:
+    mid=(left+right)//2
+
+    cnt_lan=0
+    for item in lan:
+        cnt_lan += item//mid
+
+    if cnt_lan < n:
+        right=mid-1
+    else:
+        left=mid+1
+
+print(right)
