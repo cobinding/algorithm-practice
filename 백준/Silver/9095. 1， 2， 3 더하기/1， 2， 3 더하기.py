4accde636ced8a7466416bece10b7ca16e7eca73
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-10-19 21:29:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,25 +1,13 @@
-import sys
-input = sys.stdin.readline
-t = int(input())
+import sys; input = sys.stdin.readline
 
-# 백트래킹 함수
-def back(total):
-    global response
-    
-    # 재귀 return 조건
-    if sum(total) == n:
-        response += 1
-        return
-        
-    for num in range(1,4):
-        if not total or sum(total) + num <= n :
-            total.append(num)
-            back(total)
-            total.pop()
-    
+t=int(input())
 for _ in range(t):
-    n = int(input())
-    total = []
-    response = 0
-    back(total)
-    print(response)
+    n=int(input())
+    
+    dp=[0 for _ in range(12)]
+    dp[0],dp[1],dp[2],dp[3] = 0,1,2,4
+    
+    for i in range(4, n+1):
+        dp[i]=dp[i-3]+dp[i-2]+dp[i-1]
+        
+    print(dp[n])
