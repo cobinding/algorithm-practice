065e31316cc8f0d5de6b6999067025a2cd075d46
sebin chu <102461290+cobinding@users.noreply.github.com>
2024-11-07 10:21:15 +0900

    [Silver V] Title: 최후의 승자는 누구?, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/12760. 최후의 승자는 누구？/최후의 승자는 누구？.py
+++ 백준/Silver/12760. 최후의 승자는 누구？/최후의 승자는 누구？.py
@@ -1,32 +1,18 @@
 import sys;input=sys.stdin.readline
 
 n,m = map(int, input().split())
-arr = [sorted(map(int, input().split()), reverse=True) for _ in range(n)]
+cards = [sorted(map(int, input().split()), reverse=True) for _ in range(n)]
+score = [0 for _ in range(n)]
 
-# 각 턴을 비교하기 위해 arr transport
-trans = []
 for i in range(m):
-    tmp = []
+    max_val = 0
     for j in range(n):
-        tmp.append(arr[j][i])
-    trans.append(tmp)
+        max_val = max(max_val, cards[j][i])
+    for j in range(n):
+        if cards[j][i] == max_val:
+            score[j] += 1
 
-# transport arr를 돌면서 매 턴마다 max값 확인
-# max_card와 같거나 크다면 승리 처리
-d = dict()
-for i in range(len(trans)):
-    max_card = max(trans[i])
-    for j in range(len(trans[i])):
-        if trans[i][j] >= max_card:
-            if not d.get(j+1):
-                d[j+1] = 1
-            else:
-                d[j+1] += 1
-
-ans = []
-for k, v in d.items():
-    winner = max(list(d.values()))
-    if v == winner:
-        ans.append(k)
-
-print(*sorted(ans))
+max_score = max(score)
+for i in range(n):
+    if score[i] == max_score:
+        print(i+1, end = ' ')
